f56f31905212676905ce37e263455612b9a07c42
sourabh kumar <lordgk02@gmail.com>
2025-04-14 09:00:01 +0530

    type error ignored during builds

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,14 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },  
 };
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
 
 export default nextConfig;
